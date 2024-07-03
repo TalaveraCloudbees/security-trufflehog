b9277e4a9710f9aa3edabc9bb5fb1a17c78658db
cloudbees-jtalavera <151581941+TalaveraCloudbees@users.noreply.github.com>
2024-07-03 09:02:08 +0200

    commenting main function with vulnerability

--- main1.go
+++ main1.go
@@ -3,9 +3,10 @@ package main
 import (
 	"github.com/calculi-corp/log"
 )
-
+/*
 func main() {
 	//config := `{ "aws_access_key_id":"AKIAQYLPMN5HHHFPZAM2","aws_secret_access_key": "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","region" : "us-east-2"}`
 	log.Info("Including AWS secret keys for testing")
 
 }
+*/
